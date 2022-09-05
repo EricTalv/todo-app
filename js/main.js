1cf1fb9a49aa5849b66e2adf6aa5e47d9de67dac
erictalv <eric.talviste@tptlive.ee>
2022-09-05 14:17:15 +0300

    delete & create & update

--- js/main.js
+++ js/main.js
@@ -1,99 +1,81 @@
+/**
+ * Global Variables
+ */
 
+var list = document.querySelector("#todo__list");
+var submitButton = document.querySelector("#todo__button");
+var todoInput = document.querySelector("#todo__input");
+var statusText = document.querySelector("#todo__status");
 
-// declare global variables
-var list = document.querySelector('#todo__list');
+var itemCounter = 0;
+var listItems = [];
 
-var submitButton = document.querySelector('#todo__button');
+/**
+ * Event Listeners
+ */
 
-var todoInput = document.querySelector('#todo__input');
+submitButton.addEventListener("click", addItemToList);
+list.addEventListener("click", (event) => {
+  deleteItem(event);
+  updateItem(event);
+});
 
-var statusText = document.querySelector('#todo__status');
-
-var listCounter = 0;
-
-// populate list on page load 
-// populateList();
-
-// add event listeners to inputs
-submitButton.addEventListener('click', addItemToList);
-
-todoInput.addEventListener("keypress", function(event) {
-
+// add enter-key-press functionality
+todoInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
-    
     if (!todoInput.value) {
       statusText.innerHTML = "input is empty.";
     } else {
       submitButton.click(addItemToList);
     }
   }
+});
 
-})
+/**
+ *  C R U D Operations
+ */
 
-function populateList() {
-
-  /**
-   * stretch goal:
-   * - create multiple pre lists
-   * - randomly select a pre list to be shown on page load
-   */
-
-   const preList = ['create', 'read', 'update', 'delete'];
-
-   preList.forEach(item => {
-      // create list-item element
-      const listItem = document.createElement('li');
-
-      // append todo-input value to list item element
-      listItem.appendChild(document.createTextNode(item));
-      
-      // set class for list item
-      listItem.setAttribute('class', 'todo__item');
- 
-      // append list-item to list element
-      list.appendChild(listItem);
- 
-   });
-
-  
-}
-
-// logic to add item into list 
+// C - create a new list item
 function addItemToList() {
-
-
   // check if input field is empty
   if (!todoInput.value) {
-    statusText.innerHTML = "input is empty."
-
+    statusText.innerHTML = "input is empty.";
   } else {
-    
-    listCounter++;
+    itemCounter++;
+
+    let itemValue = todoInput.value;
+    let itemId = itemCounter;
+    let itemListIndex = itemCounter - 1;
+
+    // push item to array
+
+    const item = { id: itemId, value: itemValue };
+    listItems.push(item);
 
     // create item element skeleton
-    const listItem = document.createElement('li');
-    const divWrapper = document.createElement('div');
-    const itemSpanText = document.createElement('span');
-    const itemSpanKey = document.createElement('span');
-    const divContent = document.createElement('div');
-    const divActions = document.createElement('div');
-    const anchorDelete = document.createElement('a');
-    const anchorUpdate = document.createElement('a');
+    const listItem = document.createElement("li");
+    const divWrapper = document.createElement("div");
+    const itemSpanText = document.createElement("span");
+    const divContent = document.createElement("div");
+    const divActions = document.createElement("div");
+    const anchorDelete = document.createElement("a");
+    const anchorUpdate = document.createElement("a");
 
     // add classes to elements
-    listItem.setAttribute('class', 'todo__item');
-    divWrapper.setAttribute('class', 'todo__item__wrapper');
-    itemSpanKey.setAttribute('class', 'todo__item--key');
-    itemSpanText.setAttribute('class', 'todo__item--text');
-    divContent.setAttribute('class', 'todo__item__content');
-    divActions.setAttribute('class', 'todo__item__actions');
-    anchorDelete.setAttribute('class', 'todo__item--delete');
-    anchorUpdate.setAttribute('class', 'todo__item--update');
+    listItem.setAttribute("class", "todo__item");
+    listItem.setAttribute("id", "todo__item");
+    listItem.setAttribute("data-id", itemId);
+    listItem.setAttribute("data-value", itemValue);
 
+    divWrapper.setAttribute("class", "todo__item__wrapper");
+    itemSpanText.setAttribute("class", "todo__item--text");
+    divContent.setAttribute("class", "todo__item__content");
+    divActions.setAttribute("class", "todo__item__actions");
+    anchorDelete.setAttribute("class", "todo__item--delete");
+    anchorUpdate.setAttribute("class", "todo__item--update");
 
     /* append elements */
     // todo content
-    divContent.appendChild(itemSpanKey);
     divContent.appendChild(itemSpanText);
 
     // actions and content > wrapper
@@ -106,20 +88,99 @@ function addItemToList() {
 
     // wrapper > list item
     listItem.appendChild(divWrapper);
-   
+
     // append list-item to list element
     list.appendChild(listItem);
 
     // add text values
-    itemSpanKey.innerHTML = `${listCounter}.`;
-    itemSpanText.innerHTML = todoInput.value;
-    anchorDelete.innerHTML = "x";
-    anchorUpdate.innerHTML = "o";
-
+    itemSpanText.innerHTML = itemValue;
+    anchorDelete.innerHTML = "DEL";
+    anchorUpdate.innerHTML = "UPD";
 
     // reset input & status
     todoInput.value = "";
     statusText.innerHTML = "";
   }
-
 }
+
+// D - delete item from list
+function deleteItem(event) {
+  const element = event.target;
+
+  if (element.classList[0] === "todo__item--delete") {
+    // get parent of selected item
+    const parent = element.closest("#todo__item");
+    if (parent !== null) {
+      // get values from parent
+      let dataId = parseInt(parent.getAttribute("data-id"), 10);
+      let dataValue = parent.getAttribute("data-value");
+
+      // check for current item
+      let currentlySelectedItem = listItems.find(
+        (item) => item.id === dataId && item.value === dataValue
+      );
+
+      if (currentlySelectedItem) {
+        
+
+        // find object array index
+        let itemObjIndex = listItems.indexOf(currentlySelectedItem);
+
+        // remove value from object
+        listItems.splice(itemObjIndex, 1);
+
+        // remove element from dom
+        element.closest('#todo__item').remove();
+
+
+     
+      } else {
+        console.log("object not found");
+      }
+    } else {
+      console.log("item id not found");
+    }
+  } else {
+    console.log("update button not found");
+  }
+}
+
+// U - update an existing element in the list
+function updateItem(event) {
+  const element = event.target;
+
+  if (element.classList[0] === "todo__item--update") {
+    // get parent of selected item
+    const parent = element.closest("#todo__item");
+
+    if (parent !== null) {
+      // get values from parent
+      let dataId = parseInt(parent.getAttribute("data-id"), 10);
+      let dataValue = parent.getAttribute("data-value");
+
+      // check for current item
+      let currentlySelectedItem = listItems.find(
+        (item) => item.id === dataId && item.value === dataValue
+      );
+
+      if (currentlySelectedItem) {
+        let promptValue = prompt(`Change >__${dataValue}__< to: `);
+
+        // find object array index
+        let itemObjIndex = listItems.indexOf(currentlySelectedItem);
+
+        // set value in array object
+        listItems[itemObjIndex].value = promptValue;
+
+        // set data attribute in dom
+        parent.setAttribute("data-value", promptValue);
+
+        // change value in dom
+        parent.querySelector(".todo__item--text").innerHTML = promptValue;
+      } 
+    } 
+  }
+}
+
+// Local storage
+function saveToLocalStorage() {}
